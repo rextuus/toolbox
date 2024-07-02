15b0f38d52dfb0c7b2411ff8f9df793cdda89f2f
Wolfgang Hinzmann <wolfgang.hinzmann@doccheck.com>
2024-07-02 22:37:47 +0200

    Test

---
+++ src/Command/WordleTestCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Command;
+
+use App\TimesGame\Content\Word\WordService;
+use App\TimesGame\WordleCheckService;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'wordle:test',
+    description: 'Add a short description for your command',
+)]
+class WordleTestCommand extends Command
+{
+    public function __construct(private WordleCheckService $wordService)
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $result = $this->wordService->isWordValid('young');
+
+        $output->writeln($result);
+        return Command::SUCCESS;
+    }
+}
